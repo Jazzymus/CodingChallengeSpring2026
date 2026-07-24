d9dc85c3c5fd66f9a6e086be0039f8527298670e
Jazzy <alimuhammad8999@yahoo.com>
2026-07-24 03:07:55 -0500

    Argentina bye bye

--- src/main/java/org/spring/month/Argentina.java
+++ src/main/java/org/spring/month/Argentina.java
@@ -2,6 +2,6 @@ package org.spring.month;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("Hello Ar");
+        System.out.println("Bye bye Argentina!");
     }
 }
